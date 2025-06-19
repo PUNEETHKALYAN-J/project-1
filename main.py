815169cb0f430756f2189a6637bc0941c3c8770d
PUNEETHKALYAN-J <puneethkalyan.j1@gmail.com>
2025-06-19 17:32:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-
-class MyBoxLayout(BoxLayout):
-    def on_button_click(self):
-        self.ids.my_label.text = "Button Clicked!"
+from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        return MyBoxLayout()
+        return Label(text='Hello from Kivy!')
 
 if __name__ == '__main__':
     MyApp().run()
